b2bbaf1d28c7338c33e8bad43d49b1099570416d
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-06-08 07:59:46 +0000

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
+  has_secure_password
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, length: { minimum: 8 },
+            format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar123", password_confirmation: "foobar123")
   end
 
   describe "ユーザーの新規登録" do
@@ -13,32 +14,70 @@ RSpec.describe User, type: :model do
     end
 
     context "新規登録がうまくいかないとき" do
-      it "名前が空のときは登録出来ないこと" do
+      it "nameが空のときは登録出来ないこと" do
         @user.name = "     "
         expect(@user).not_to be_valid
       end
 
-      it "メールアドレスが空のときは登録出来ないこと" do
+      it "emailが空のときは登録出来ないこと" do
         @user.email = "     "
         expect(@user).not_to be_valid
       end
 
-      it "名前が31文字以上の場合は登録出来ないこと" do
+      it "nameが31文字以上の場合は登録出来ないこと" do
         @user.name = "a" * 31
         expect(@user).not_to be_valid
       end
 
-      it "メールアドレスが256文字以上の場合は登録出来ないこと" do
+      it "emailが256文字以上の場合は登録出来ないこと" do
         @user.email = "a" * 244 + "@example.com"
         expect(@user).not_to be_valid
       end
 
-      it "重複したメールアドレスは登録出来ないこと" do
+      it "重複したemailは登録出来ないこと" do
         duplicate_user = @user.dup
-        duplicate_user.email = @user.email.upcase
         @user.save
         expect(duplicate_user).not_to be_valid
       end
+
+      it "passwordが空では登録出来ないこと" do
+        @user.password = @user.password_confirmation = ""
+        expect(@user).not_to be_valid
+      end
+
+      it "passwordが7文字以下では登録出来ないこと" do
+        @user.password = @user.password_confirmation = "foobar1"
+        expect(@user).not_to be_valid
+      end
+
+      it "passwordが存在してもpassword_confirmationが空では登録出来ないこと" do
+        @user.password_confirmation = ""
+        expect(@user).not_to be_valid
+      end
+
+      it "passwordは半角英数字混合でなければ登録出来ないこと" do
+        @user.password = @user.password_confirmation = "foobarbaz"
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "コールバックメソッドの検証" do
+    it "メールアドレスは小文字で保存されること" do
+      @user.email = "Foo@ExAMPle.CoM"
+      @user.save
+      expect(@user.reload.email).to eq "foo@example.com"
+    end
+  end
+
+  describe "passwordの検証" do
+    it "passwordとpassword_confirmationが一致しない場合は無効となること" do
+      @user.password_confirmation = "foobar456"
+      expect(@user).not_to be_valid
+    end
+
+    it 'passwordが暗号化されていること' do
+      expect(@user.encrypted_attributes).not_to eq "foobar123"
     end
   end
 end
