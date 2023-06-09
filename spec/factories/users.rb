a43ec83b61a9308b219aa26c1d0f6f722bb3e984
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-06-09 08:36:17 +0000

    Finish User signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    name                  { "Sample User" }
+    email                 { "rails@example.com" }
+    password              { "foobar123" }
+    password_confirmation { "foobar123" }
   end
 end
