e397d0fa29f5c0d9a39330fb51f4cb9febb6455e
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-06-18 11:48:54 +0000

    account activations#editの記述・sessions#createの修正

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "アカウント有効化に成功しました"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = "無効な有効化URLです"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      log_in user
-      flash[:success] = "ログインしました"
-      redirect_to forwarding_url || user_path(user)
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user_path(user)
+      else
+        flash[:warning] = "アカウントを有効化していません。有効化メールを確認してください"
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = "メールアドレスまたはパスワードが違います"
       render 'new', status: :unprocessable_entity
