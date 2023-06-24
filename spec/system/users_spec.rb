419062100c9acff64e5c47ebb51d42c37e05534a
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-06-24 12:08:26 +0000

    ユーザー登録時の画像設定のテストを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,11 +1,28 @@
 RSpec.describe "Users", type: :system do
 
-  describe "#create" do
+  describe "#new, #create" do
+    context "有効な値の場合" do
+      it "ユーザー登録時にアカウント画像が設定できること" do
+        visit signup_path
+        expect {
+          fill_in "名前", with: "Valid Man"
+          fill_in "メールアドレス", with: "user@valid.com"
+          fill_in "パスワード", with: "foobar123"
+          fill_in "確認用パスワード", with: "foobar123"
+          attach_file "user[image]", "#{Rails.root}/spec/factories/kitten.jpg"
+          click_button "登録する"
+        }.to change(User, :count).by 1
+
+        attached_user = User.last
+        expect(attached_user.image).to be_attached
+      end
+    end
+
     context "無効な値の場合" do
       it "エラーメッセージ用の表示領域が描画されていること" do
         visit signup_path
         fill_in "名前", with: "Invalid Man"
-        fill_in "メールアドレス", with: "user@invlid.com"
+        fill_in "メールアドレス", with: "user@invalid.com"
         fill_in "パスワード", with: "foobar"
         fill_in "確認用パスワード", with: "barbaz"
         click_button "登録する"
