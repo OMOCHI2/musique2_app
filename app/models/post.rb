3953dfb23755bcc5728798f70160b5d9e2fc5e3c
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-07-04 13:43:36 +0000

    system/posts_specを追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,6 @@ class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  with_options presence: true, on: :publicize do
-    validates :title
-    validates :content
-  end
-  validates :title,   length: { maximum: 255 },   on: :publicize
+  validates :title,   presence: true, length: { maximum: 255 },   on: :publicize
   validates :content, length: { maximum: 10000 }, on: :publicize
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,6 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :system do
+  describe "#new, #create", js: true do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      log_in user
+      visit new_post_path
+    end
+    context "有効な場合" do
+      it "記事を公開できること", driver: :selenium_chrome_headless do
+        expect {
+          fill_in "post_title", with: "Sample title"
+          find(".trix-content").set("Sample content")
+          click_on "公開する"
+        }.to change(Post, :count).by 1
+
+        post = Post.last
+        expect(post.is_draft).to be_falsey
+      end
+
+      it "記事を下書きとして非公開で保存できること" do
+        expect {
+          fill_in "post_title", with: "Sample title"
+          find(".trix-content").set("Sample content")
+          click_on "下書き保存"
+        }.to change(Post, :count).by 1
+
+        post = Post.last
+        expect(post.is_draft).to be_truthy
+      end
+    end
+
+    context "無効な場合" do
+      it "エラーメッセージ用の表示領域が描画されていること" do
+        expect {
+          fill_in "post_title", with: ""
+          find(".trix-content").set("")
+          click_on "公開する"
+        }.not_to change(Post, :count)
+
+        expect(page).to have_selector "div#error_explanation"
+        expect(page).to have_selector "div.field_with_errors"
+      end
+    end
+  end
+
+  describe "#edit, #update" do
+    context "公開済み記事の場合" do
+      it "有効な値のときは更新できること" do
+      end
+
+      it "無効な値のときは更新されず、エラーメッセージが表示されること" do
+      end
+    end
+
+    context "下書き記事の場合" do
+      it "有効な値のときは公開できること" do
+      end
+
+      it "有効な値のときは下書きを更新できること" do
+      end
+
+      it "無効な値のときは更新されず、エラーメッセージが表示されること" do
+      end
+    end
+  end
+
   describe "Users#show" do
     before do
       FactoryBot.send(:user_with_posts, posts_count: 35)
