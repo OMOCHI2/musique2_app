f47d282667444c40d505438b1ddf8b58c9eee409
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-07-22 14:18:23 +0000

    user/following, followersページのデザイン変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
   get    "/about",   to: "static_pages#about"
   get    "/terms",   to: "static_pages#terms"
   get    "/privacy", to: "static_pages#privacy"
-  # get    "/contact", to: "static_pages#contact"
   get    "/signup",  to: "users#new"
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
 
   get    "/contact",         to: "contacts#index"
-  post    "/contact/confirm", to: "contacts#confirm"
-  post    "/contact/done",    to: "contacts#done"
+  post   "/contact/confirm", to: "contacts#confirm"
+  post   "/contact/done",    to: "contacts#done"
 
   post   "/google_login_api/callback", to: "google_login_api#callback"
 
