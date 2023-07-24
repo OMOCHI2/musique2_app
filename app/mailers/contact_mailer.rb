5ba5c012ba0ae822a255aaed528b9f35d72229f4
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-07-24 13:21:45 +0000

    contact/send_mailの文面を変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,10 @@
 class ContactMailer < ApplicationMailer
-  default from: "noreply@example.com"
-  default to: "admin@example.com"
-  layout "mailer"
-
   def send_mail(contact)
     @contact = contact
-    mail(from: contact.email, to: ENV['MAILGUN_SMTP_LOGIN'],
-                              subject: "Webサイト「MUSIQUE」より問い合わせが届きました") do |format|
-      format.text
-    end
+    mail(
+      from: contact.email,
+      to: ENV['ADMIN_EMAIL'],
+      subject: "Webサイト「MUSIQUE」より問い合わせが届きました"
+    )
   end
 end
