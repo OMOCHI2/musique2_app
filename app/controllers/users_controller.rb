f216ea8aa4467b7e952990f726a8cfb50d36f3c7
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-07-31 17:44:03 +0000

    Postのscopeの変更。デフォルトは登録順、descendingメソッドで降順に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_path and return unless @user.activated?
-    @posts = @user.posts.where(is_draft: false).paginate(page: params[:page])
+    @posts = @user.posts.where(is_draft: false).descending.paginate(page: params[:page])
   end
 
   def new
