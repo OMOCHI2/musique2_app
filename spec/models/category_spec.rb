73df3dfe4a909513a90e0394d0dd993776cdf43d
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-08-02 14:05:48 +0000

    models/category テスト記述

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { Category.new(name: "sample") }
+
+  it "有効であること" do
+    expect(category).to be_valid
+  end
+
+  it "カテゴリー名が空の場合は無効であること" do
+    category.name = ""
+    expect(category).not_to be_valid
+  end
 end
