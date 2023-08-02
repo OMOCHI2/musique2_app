c7adafabbf8e2657f206a2d7dc104d0ff18b26ba
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-08-02 02:03:01 +0000

    タイトル表示を大文字にしたため、動的タイトルテストを修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
     it "引数が渡されている場合に動的な表示となること" do
-      expect(full_title("hoge")).to eq "hoge - Musique"
+      expect(full_title("hoge")).to eq "hoge - MUSIQUE"
     end
 
     it "引数が空白の場合に動的な表示となること" do
-      expect(full_title("")).to eq "Musique"
+      expect(full_title("")).to eq "MUSIQUE"
     end
 
     it "引数がnilの場合に動的な表示となること" do
-      expect(full_title(nil)).to eq "Musique"
+      expect(full_title(nil)).to eq "MUSIQUE"
     end
   end
 end
